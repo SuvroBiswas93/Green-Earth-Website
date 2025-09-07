d71cabcf7d207031e129cf3bef6c11e38fd51ae4
Suvro Biswas <shuvo.249963@gmail.com>
2025-09-08 03:15:33 +0600

    Implement default page data and show it inside the dom

--- script/script.js
+++ script/script.js
@@ -1,7 +1,7 @@
 const categoriesContainer = document.getElementById('category-container')
 
+const cardContainer = document.getElementById('card-container')
 
-// load the category
 
 const loadCategory = () =>{
     fetch(`https://openapi.programming-hero.com/api/categories`)
@@ -12,15 +12,47 @@ const loadCategory = () =>{
 
 const displayCategoryData = (categoryName =>{
     categoryName.forEach(name => {
-        console.log(name.category_name)
         categoriesContainer.innerHTML+= `
                     <div>
                         <ul class="py-1">
-                            <button class = " px-1 text-left hover:bg-green-500 w-full rounded cursor-pointer">${name.category_name}</button>
+                            <button class = " px-1 text-left hover:bg-green-500 active:bg-green-600 w-full rounded cursor-pointer">${name.category_name}</button>
                         </ul>
                     </div>
         `
     });
 })
 
+const loadDefaultPageData = () =>{
+    const url = `https://openapi.programming-hero.com/api/plants`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayDefaultPageData(data.plants))
+    .catch(err => console.log("Data fetching error..."))
+}
+
+const displayDefaultPageData = (defaultData) =>{
+    for (const data of defaultData) {
+        cardContainer.innerHTML += `
+             <div class="card space-y-3 p-3 bg-white rounded-t">
+                        <img src="${data.image}" alt="" class= "rounded-t">
+                            
+                        <div class="px-2">
+                                <h1 class="font-bold">${data.name}</h1>
+                                <p class ="overflow-y-auto">${data.description}</p>
+                        </div>
+                        <div class="flex justify-between px-2">
+                                <button id="" class="bg-[#DCFCE7] text-sm p-2 rounded-3xl">${data.category}</button>
+                                <p><i class="fa-solid fa-bangladeshi-taka-sign"></i><span id="amount">${data.price}</span></p>
+
+                        </div>
+                        <div>
+                            <button id="add-card-btn" class="btn btn-info bg-green-500 border-none w-full px-2 text-center rounded-3xl text-white">Add to cart</button>
+                        </div>
+                    </div>
+        
+        `
+    }
+}
+
 loadCategory()
+loadDefaultPageData()
