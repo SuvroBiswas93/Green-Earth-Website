9f13825ca622d60c514e76772559b4b8c96a6497
Suvro Biswas <shuvo.249963@gmail.com>
2025-09-08 01:33:22 +0600

    implement category feature and show it inside DOM

--- script/script.js
+++ script/script.js
@@ -1,1 +1,26 @@
-console.log('hello from assignment 6')
+const categoriesContainer = document.getElementById('category-container')
+
+
+// load the category
+
+const loadCategory = () =>{
+    fetch(`https://openapi.programming-hero.com/api/categories`)
+    .then(res => res.json())
+    .then(data => displayCategoryData(data.categories))
+    .catch(err => console.log('data Fetching error'))
+}
+
+const displayCategoryData = (categoryName =>{
+    categoryName.forEach(name => {
+        console.log(name.category_name)
+        categoriesContainer.innerHTML+= `
+                    <div>
+                        <ul class="py-1">
+                            <button class = " px-1 text-left hover:bg-green-500 w-full rounded cursor-pointer">${name.category_name}</button>
+                        </ul>
+                    </div>
+        `
+    });
+})
+
+loadCategory()
